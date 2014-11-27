ba0a89a414005fdf50f4e78a47855cc31ee09f9c
Lauri Taimila <lauri@taimila.com>
2014-11-27 14:22:36 +0200

    Step 3. Persistence

---
+++ Domain/Persistence/InvoiceRepository.cs
@@ -0,0 +1,35 @@
+﻿using EnterpriseApplication.Invoice;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System;
+using System.Linq;
+
+namespace Persistence
+{
+    public class InvoiceRepository : MongoRepository, IInvoiceRepository
+    {
+        public InvoiceRepository(MongoClient mongo) : base(mongo) { }
+
+        public IInvoice GetById(Guid id)
+        {
+            return Invoices.AsQueryable<Invoice>()
+                           .Single(c => c.Id == id);
+        }
+
+        public IInvoice GetByNumber(string invoiceNumber)
+        {
+            return Invoices.AsQueryable<Invoice>()
+                           .Single(c => c.Number == invoiceNumber);
+        }
+
+        public void Save(IInvoice invoice)
+        {
+            Invoices.Save(invoice);
+        }
+
+        MongoCollection<IInvoice> Invoices
+        {
+            get { return Database.GetCollection<IInvoice>("invoices"); }
+        }
+    }
+}
---
+++ Domain/Persistence/MongoRepository.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+
+namespace Persistence
+{
+    public class MongoRepository
+    {
+        readonly MongoClient mongo;
+
+        public MongoRepository(MongoClient mongo)
+        {
+            this.mongo = mongo;
+        }
+
+        protected MongoDatabase Database
+        {
+            get
+            {
+                var server = mongo.GetServer();
+                return server.GetDatabase("EnterpriseApplication");
+            }
+        }
+    }
+}
